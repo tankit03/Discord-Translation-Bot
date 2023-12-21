d25e4e0bb42d61a5155e1860820df19db8e0fd39
tankit03 <huparetanish@gmail.com>
2023-12-20 23:01:15 -0800

    styling

--- src/commands.js
+++ src/commands.js
@@ -22,44 +22,6 @@ const commands = [
             },
         ],
     },
-
-    // {
-    //     name: 'spanish',
-    //     description: 'will translate english to spanish for you 🇪🇸 ',
-    //     options: [
-    //         {
-    //             name: 'text',
-    //             description: 'The text you want to translate to Spanish 🇪🇸',
-    //             type: ApplicationCommandOptionType.String,
-    //             required: true,
-    //         },
-    //     ],
-    // },
-    // {
-    //     name: 'french',
-    //     description: 'will translate english to French for you 🇫🇷 ',
-    //     options: [
-    //         {
-    //             name: 'text',
-    //             description: 'The text you want to translate to French 🇫🇷',
-    //             type: ApplicationCommandOptionType.String,
-    //             required: true,
-    //         },
-    //     ],
-    // },
-    // {
-    //     name: 'hindi',
-    //     description: 'will translate english to Hindi for you 🇮🇳 ',
-    //     options: [
-    //         {
-    //             name: 'text',
-    //             description: 'The text you want to translate to Hindi 🇮🇳',
-    //             type: ApplicationCommandOptionType.String,
-    //             required: true,
-    //         },
-    //     ],
-    // },
-
 ];
 
 const rest = new REST({ version: '10'}).setToken(process.env.TOKEN);
